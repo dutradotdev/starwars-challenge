dc7d21cb87361ae0f85132112caadf29bf107c3a
LUCAS DUTRA <lucas.dutra@midway.com.br>
2021-05-17 20:50:07 -0300

    chore: reactotron start

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
+/* global __DEV__ */
 import React from 'react'
 import 'react-native-gesture-handler'
 import Router from './src/router'
+
+if (__DEV__) {
+  import('./ReactotronConfig').then(() => console.log('Reactotron Configured'))
+}
 
 const App = () => <Router />
 
