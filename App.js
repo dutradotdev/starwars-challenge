b43b02b75ad31c7442f428604e08bcf8f57e0930
LUCAS DUTRA <lucas.dutra@midway.com.br>
2021-05-18 11:47:51 -0300

    chore: ignore warnings from react-native-avataaars

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 /* global __DEV__ */
 import React from 'react'
+import { LogBox } from 'react-native'
 import 'react-native-gesture-handler'
 import Router from './src/router'
 
 if (__DEV__) {
   import('./ReactotronConfig').then(() => console.log('Reactotron Configured'))
 }
+
+// ignore warnings from react-native-avataaars
+LogBox.ignoreAllLogs()
 
 const App = () => <Router />
 
